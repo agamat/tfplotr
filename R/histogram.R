2e53ad393e940e976094a7368e347e2afa4276db
Josie Kressner <josie@transportfoundry.com>
2016-09-08 19:17:24 -0400

    Skeleton for histogram of trip rates and tour rates per person

---
+++ R/histogram.R
@@ -0,0 +1,11 @@
+#' Plot histogram of trips per person.
+#'
+#' @param tbl tbl of data with at least the following columns: .
+#'
+#' @return A plot.
+#'
+#' @export
+plot_trips_per_person <- function(tbl) {
+
+
+}
